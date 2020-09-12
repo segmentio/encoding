4677ae6be9d21d15b8538503aff65055eefc6c19
Achille <achille@segment.com>
2020-09-12 00:38:30 -0700

    Update proto/size.go
    Co-authored-by: Chris O'Hara <cohara87@gmail.com>

--- proto/size.go
+++ proto/size.go
@@ -12,7 +12,7 @@ func sizeOfVarint(v uint64) int {
 }
 
 func sizeOfVarintZigZag(v int64) int {
-	return sizeOfVarint(uint64((v << 1) ^ (v >> 63)))
+	return sizeOfVarint((uint64(v) << 1) ^ uint64(v >> 63))
 }
 
 func sizeOfVarlen(n int) int {
