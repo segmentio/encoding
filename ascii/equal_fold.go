0bfa9d2b9be86e1efa1b56434d543049a36ec155
Achille Roussel <achille@segment.com>
2021-03-22 18:41:06 -0700

    optimize the implementation

--- ascii/equal_fold.go
+++ ascii/equal_fold.go
@@ -1,5 +1,7 @@
 package ascii
 
+import "unsafe"
+
 // EqualFold is a version of bytes.EqualFold designed to work on ASCII input
 // instead of UTF-8.
 //
@@ -27,10 +29,52 @@ func EqualFoldString(a, b string) bool {
 		return false
 	}
 
-	for i := 0; i < len(a); i++ {
-		if (a[i] & 0xDF) != (b[i] & 0xDF) {
+	n := uintptr(len(a))
+	p := *(*unsafe.Pointer)(unsafe.Pointer(&a))
+	q := *(*unsafe.Pointer)(unsafe.Pointer(&b))
+	r := unsafe.Pointer(uintptr(p) + n)
+
+	if n >= 8 {
+		k := (n / 8) * 8
+		x := unsafe.Pointer(uintptr(p) + k)
+
+		for uintptr(p) < uintptr(x) {
+			const mask = 0xDFDFDFDFDFDFDFDF
+
+			if (*(*uint64)(p) & mask) != (*(*uint64)(q) & mask) {
+				return false
+			}
+
+			p = unsafe.Pointer(uintptr(p) + 8)
+			q = unsafe.Pointer(uintptr(q) + 8)
+		}
+	}
+
+	if (uintptr(r) - uintptr(p)) >= 4 {
+		const mask = 0xDFDFDFDF
+
+		if (*(*uint32)(p) & mask) != (*(*uint32)(q) & mask) {
 			return false
 		}
+
+		p = unsafe.Pointer(uintptr(p) + 4)
+		q = unsafe.Pointer(uintptr(q) + 4)
+	}
+
+	if (uintptr(r) - uintptr(p)) >= 2 {
+		const mask = 0xDFDF
+
+		if (*(*uint16)(p) & mask) != (*(*uint16)(q) & mask) {
+			return false
+		}
+
+		p = unsafe.Pointer(uintptr(p) + 2)
+		q = unsafe.Pointer(uintptr(q) + 2)
+	}
+
+	if uintptr(p) < uintptr(r) {
+		const mask = 0xDF
+		return (*(*uint8)(p) & mask) == (*(*uint8)(q) & mask)
 	}
 
 	return true
