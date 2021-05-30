fb8b5ddcefbb240c22e80b93292ec16fba393f23
Jeremy Larkin <jeremy.larkin@segment.com>
2021-05-30 12:30:50 -0700

    add fixed default implementation and test

--- ascii/equal_fold.go
+++ ascii/equal_fold.go
@@ -1,9 +1,7 @@
 //go:generate go run equal_fold_asm.go -out equal_fold_amd64.s -stubs equal_fold_amd64.go
 package ascii
 
-import (
-	"unsafe"
-)
+import "unsafe"
 
 // EqualFold is a version of bytes.EqualFold designed to work on ASCII input
 // instead of UTF-8.
@@ -35,26 +33,22 @@ func EqualFoldString(a, b string) bool {
 	n := uintptr(len(a))
 	p := *(*unsafe.Pointer)(unsafe.Pointer(&a))
 	q := *(*unsafe.Pointer)(unsafe.Pointer(&b))
+	c := byte(0)
+
 	// Pre-check to avoid the other tests that would all evaluate to false.
 	// For very small strings, this helps reduce the processing overhead.
 	if n >= 8 {
-		// If there is more than 32 bytes to copy, use the AVX optimized version,
-		// otherwise the overhead of the function call tends to be greater than
-		// looping 2 or 3 times over 8 bytes.
-		if n > 32 && asm.equalFoldAVX2 != nil {
-			if asm.equalFoldAVX2((*byte)(p), (*byte)(q), n) == 0 {
-				return false
-			}
-			k := (n / 16) * 16
-			p = unsafe.Pointer(uintptr(p) + k)
-			q = unsafe.Pointer(uintptr(q) + k)
-			n -= k
-		}
+		for n >= 8 {
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 0))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 0))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 1))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 1))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 2))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 2))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 3))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 3))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 4))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 4))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 5))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 5))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 6))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 6))]
+			c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 7))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 7))]
 
-		for n > 8 {
-			const mask = 0xDFDFDFDFDFDFDFDF
-
-			if (*(*uint64)(p) & mask) != (*(*uint64)(q) & mask) {
+			if c != 0 {
 				return false
 			}
 
@@ -62,39 +56,33 @@ func EqualFoldString(a, b string) bool {
 			q = unsafe.Pointer(uintptr(q) + 8)
 			n -= 8
 		}
-
-		if n == 8 {
-			const mask = 0xDFDFDFDFDFDFDFDF
-			return (*(*uint64)(p) & mask) == (*(*uint64)(q) & mask)
-		}
-	}
-
-	if n > 4 {
-		const mask = 0xDFDFDFDF
-
-		if (*(*uint32)(p) & mask) != (*(*uint32)(q) & mask) {
-			return false
-		}
-
-		p = unsafe.Pointer(uintptr(p) + 4)
-		q = unsafe.Pointer(uintptr(q) + 4)
-		n -= 4
 	}
 
 	switch n {
+	case 7:
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 6))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 6))]
+		fallthrough
+	case 6:
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 5))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 5))]
+		fallthrough
+	case 5:
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 4))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 4))]
+		fallthrough
 	case 4:
-		return (*(*uint32)(p) & 0xDFDFDFDF) == (*(*uint32)(q) & 0xDFDFDFDF)
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 3))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 3))]
+		fallthrough
 	case 3:
-		x := uint32(*(*uint16)(p)) | uint32(*(*uint8)(unsafe.Pointer(uintptr(p) + 2)))<<16
-		y := uint32(*(*uint16)(q)) | uint32(*(*uint8)(unsafe.Pointer(uintptr(q) + 2)))<<16
-		return (x & 0xDFDFDF) == (y & 0xDFDFDF)
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 2))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 2))]
+		fallthrough
 	case 2:
-		return (*(*uint16)(p) & 0xDFDF) == (*(*uint16)(q) & 0xDFDF)
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 1))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 1))]
+		fallthrough
 	case 1:
-		return (*(*uint8)(p) & 0xDF) == (*(*uint8)(q) & 0xDF)
-	default:
-		return true
+		c |= lower[*(*uint8)(unsafe.Pointer(uintptr(p) + 0))] ^ lower[*(*uint8)(unsafe.Pointer(uintptr(q) + 0))]
 	}
+
+	return c == 0
+
 }
 
 func HasPrefixFoldString(s, prefix string) bool {
