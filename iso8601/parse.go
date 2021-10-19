cc0871ed591b78c2848ffe7512dc8aaa8324d824
Chris O'Hara <cohara87@gmail.com>
2021-10-19 12:31:33 +1000

    Nanosecond specialization for iso8601.Parse (#104)

--- iso8601/parse.go
+++ iso8601/parse.go
@@ -18,11 +18,15 @@ var (
 
 // Parse parses an ISO8601 timestamp, e.g. "2021-03-25T21:36:12Z".
 func Parse(input string) (time.Time, error) {
-	switch b := unsafeStringToBytes(input); len(b) {
-	case 20: // YYYY-MM-DDTHH:MM:SSZ
+	b := unsafeStringToBytes(input)
+	if len(b) >= 20 && len(b) <= 30 && b[len(b)-1] == 'Z' {
+		if len(b) == 21 || (len(b) > 21 && b[19] != '.') {
+			return time.Time{}, errInvalidTimestamp
+		}
+
 		t1 := binary.LittleEndian.Uint64(b)
-		t2 := binary.LittleEndian.Uint64(b[8:])
-		t3 := uint64(binary.LittleEndian.Uint32(b[16:]))
+		t2 := binary.LittleEndian.Uint64(b[8:16])
+		t3 := uint64(b[16]) | uint64(b[17])<<8 | uint64(b[18])<<16 | uint64('Z')<<24
 
 		// Check for valid separators by masking input with "    -  -  T  :  :  Z".
 		// If separators are all valid, replace them with a '0' (0x30) byte and
@@ -47,60 +51,43 @@ func Parse(input string) (time.Time, error) {
 		minute := (t2>>48&0xF)*10 + (t2 >> 56)
 		second := (t3>>8&0xF)*10 + (t3 >> 16)
 
-		if err := validate(year, month, day, hour, minute, second); err != nil {
-			return time.Time{}, err
+		nanos := int64(0)
+		if len(b) > 20 {
+			for _, c := range b[20 : len(b)-1] {
+				if c < '0' || c > '9' {
+					return time.Time{}, errInvalidTimestamp
+				}
+				nanos = (nanos * 10) + int64(c-'0')
+			}
+			nanos *= pow10[30-len(b)]
 		}
-
-		unixSeconds := int64(daysSinceEpoch(year, month, day))*86400 + int64(hour*3600+minute*60+second)
-		return time.Unix(unixSeconds, 0).UTC(), nil
-
-	case 24: // YYYY-MM-DDTHH:MM:SS.MMMZ
-		t1 := binary.LittleEndian.Uint64(b)
-		t2 := binary.LittleEndian.Uint64(b[8:])
-		t4 := binary.LittleEndian.Uint64(b[16:])
-
-		// Check for valid separators by masking input with "    -  -  T  :  :  .   Z".
-		// If separators are all valid, replace them with a '0' (0x30) byte and
-		// check all bytes are now numeric.
-		if !match(t1, mask1) || !match(t2, mask2) || !match(t4, mask4) {
-			// Note that there
-			return time.Time{}, errInvalidTimestamp
-		}
-		t1 ^= replace1
-		t2 ^= replace2
-		t4 ^= replace4
-		if (nonNumeric(t1) | nonNumeric(t2) | nonNumeric(t4)) != 0 {
-			return time.Time{}, errInvalidTimestamp
-		}
-
-		t1 -= zero
-		t2 -= zero
-		t4 -= zero
-		year := (t1&0xF)*1000 + (t1>>8&0xF)*100 + (t1>>16&0xF)*10 + (t1 >> 24 & 0xF)
-		month := (t1>>40&0xF)*10 + (t1 >> 48 & 0xF)
-		day := (t2&0xF)*10 + (t2 >> 8 & 0xF)
-		hour := (t2>>24&0xF)*10 + (t2 >> 32 & 0xF)
-		minute := (t2>>48&0xF)*10 + (t2 >> 56)
-		second := (t4>>8&0xF)*10 + (t4 >> 16 & 0xF)
-		millis := (t4>>32&0xF)*100 + (t4>>40&0xF)*10 + (t4 >> 48)
 
 		if err := validate(year, month, day, hour, minute, second); err != nil {
 			return time.Time{}, err
 		}
 
 		unixSeconds := int64(daysSinceEpoch(year, month, day))*86400 + int64(hour*3600+minute*60+second)
-		return time.Unix(unixSeconds, int64(millis*1e6)).UTC(), nil
-
-	default:
-		return time.Parse(time.RFC3339Nano, input)
+		return time.Unix(unixSeconds, nanos).UTC(), nil
 	}
+
+	// Fallback to using time.Parse().
+	t, err := time.Parse(time.RFC3339Nano, input)
+	if err != nil {
+		// Override (and don't wrap) the error here. The error returned by
+		// time.Parse() is dynamic, and includes a reference to the input
+		// string. By overriding the error, we guarantee that the input string
+		// doesn't escape.
+		return time.Time{}, errInvalidTimestamp
+	}
+	return t, nil
 }
+
+var pow10 = []int64{1, 10, 100, 1000, 1e4, 1e5, 1e6, 1e7, 1e8}
 
 const (
 	mask1 = 0x2d00002d00000000 // YYYY-MM-
 	mask2 = 0x00003a0000540000 // DDTHH:MM
 	mask3 = 0x000000005a00003a // :SSZ____
-	mask4 = 0x5a0000002e00003a // :SS.MMMZ
 
 	// Generate masks that replace the separators with a numeric byte.
 	// The input must have valid separators. XOR with the separator bytes
@@ -108,7 +95,6 @@ const (
 	replace1 = mask1 ^ 0x3000003000000000
 	replace2 = mask2 ^ 0x0000300000300000
 	replace3 = mask3 ^ 0x3030303030000030
-	replace4 = mask4 ^ 0x3000000030000030
 
 	lsb = ^uint64(0) / 255
 	msb = lsb * 0x80
--- iso8601/parse_test.go
+++ iso8601/parse_test.go
@@ -24,13 +24,42 @@ func TestParse(t *testing.T) {
 		"0000-01-01T00:00:00.000Z",
 		"9999-12-31T23:59:59.999Z",
 
-		// Slow path
-		"2006-01-02T15:04:05+00:00",
-		"2006-01-02T15:04:05-07:00",
-		"2006-01-02T15:04:05.123+10:00",
-		"2006-01-02T15:04:05.123-08:00",
-		"2006-01-02T15:04:05.123456Z",
+		// Fast path (30 bytes)
+		"1987-12-16T23:45:12.123456789Z",
 		"2006-01-02T15:04:05.123456789Z",
+		"2000-02-29T23:59:59.123456789Z",
+		"2020-02-29T23:59:59.123456789Z",
+		"0000-01-01T00:00:00.000000000Z",
+		"9999-12-31T23:59:59.999999999Z",
+
+		// Slow path
+		"2006-01-02T15:04:05.1Z",
+		"2006-01-02T15:04:05.12Z",
+		"2006-01-02T15:04:05.1234Z",
+		"2006-01-02T15:04:05.12345Z",
+		"2006-01-02T15:04:05.123456Z",
+		"2006-01-02T15:04:05.1234567Z",
+		"2006-01-02T15:04:05.12345678Z",
+		"2021-10-16T07:55:07+10:00",
+		"2021-10-16T07:55:07.1+10:00",
+		"2021-10-16T07:55:07.12+10:00",
+		"2021-10-16T07:55:07.123+10:00",
+		"2021-10-16T07:55:07.1234+10:00",
+		"2021-10-16T07:55:07.12345+10:00",
+		"2021-10-16T07:55:07.123456+10:00",
+		"2021-10-16T07:55:07.1234567+10:00",
+		"2021-10-16T07:55:07.12345678+10:00",
+		"2021-10-16T07:55:07.123456789+10:00",
+		"2021-10-16T07:55:07-10:00",
+		"2021-10-16T07:55:07.1-10:00",
+		"2021-10-16T07:55:07.12-10:00",
+		"2021-10-16T07:55:07.123-10:00",
+		"2021-10-16T07:55:07.1234-10:00",
+		"2021-10-16T07:55:07.12345-10:00",
+		"2021-10-16T07:55:07.123456-10:00",
+		"2021-10-16T07:55:07.1234567-10:00",
+		"2021-10-16T07:55:07.12345678-10:00",
+		"2021-10-16T07:55:07.123456789-10:00",
 	} {
 		t.Run(input, func(t *testing.T) {
 			expect, err := time.Parse(time.RFC3339Nano, input)
@@ -80,11 +109,27 @@ func TestParse(t *testing.T) {
 		}
 	}
 
+	// Check ~4M YYYY-MM-DD dates in 30 byte form.
+	for year := 0; year <= 9999; year++ {
+		for month := 0; month <= 13; month++ {
+			for day := 0; day <= 32; day++ {
+				input := fmt.Sprintf("%04d-%02d-%02dT12:34:56.123456789Z", year, month, day)
+				expect, expectErr := time.Parse(time.RFC3339Nano, input)
+				actual, actualErr := Parse(input)
+				if (expectErr != nil) != (actualErr != nil) {
+					t.Errorf("unexpected error for %v: %v vs. %v expected", input, actualErr, expectErr)
+				} else if !actual.Equal(expect) {
+					t.Errorf("unexpected time for %v: %v vs. %v expected", input, actual, expect)
+				}
+			}
+		}
+	}
+
 	// Check all ~1M HH:MM:SS times in 20 byte form.
 	for hour := 0; hour < 100; hour++ {
 		for minute := 0; minute < 100; minute++ {
 			for second := 0; second < 100; second++ {
-				input := fmt.Sprintf("2000-01-01T%02d-%02d-%02dZ", hour, minute, second)
+				input := fmt.Sprintf("2000-01-01T%02d:%02d:%02dZ", hour, minute, second)
 				expect, expectErr := time.Parse(time.RFC3339Nano, input)
 				actual, actualErr := Parse(input)
 				if (expectErr != nil) != (actualErr != nil) {
@@ -100,7 +145,7 @@ func TestParse(t *testing.T) {
 	for hour := 0; hour < 100; hour++ {
 		for minute := 0; minute < 100; minute++ {
 			for second := 0; second < 100; second++ {
-				input := fmt.Sprintf("2000-01-01T%02d-%02d-%02d.123Z", hour, minute, second)
+				input := fmt.Sprintf("2000-01-01T%02d:%02d:%02d.123Z", hour, minute, second)
 				expect, expectErr := time.Parse(time.RFC3339Nano, input)
 				actual, actualErr := Parse(input)
 				if (expectErr != nil) != (actualErr != nil) {
@@ -109,6 +154,58 @@ func TestParse(t *testing.T) {
 					t.Errorf("unexpected time for %v: %v vs. %v expected", input, actual, expect)
 				}
 			}
+		}
+	}
+
+	// Check ~1M HH:MM:SS.MMM times in 30 byte form.
+	for hour := 0; hour < 100; hour++ {
+		for minute := 0; minute < 100; minute++ {
+			for second := 0; second < 100; second++ {
+				input := fmt.Sprintf("2000-01-01T%02d:%02d:%02d.123456789Z", hour, minute, second)
+				expect, expectErr := time.Parse(time.RFC3339Nano, input)
+				actual, actualErr := Parse(input)
+				if (expectErr != nil) != (actualErr != nil) {
+					t.Errorf("unexpected error for %v: %v vs. %v expected", input, actualErr, expectErr)
+				} else if !actual.Equal(expect) {
+					t.Errorf("unexpected time for %v: %v vs. %v expected", input, actual, expect)
+				}
+			}
+		}
+	}
+
+	// Check milliseconds.
+	for millis := 1; millis < 1000; millis <<= 1 {
+		input := fmt.Sprintf("2000-01-01T00:00:00.%03dZ", millis)
+		expect, expectErr := time.Parse(time.RFC3339Nano, input)
+		actual, actualErr := Parse(input)
+		if (expectErr != nil) != (actualErr != nil) {
+			t.Errorf("unexpected error for %v: %v vs. %v expected", input, actualErr, expectErr)
+		} else if !actual.Equal(expect) {
+			t.Errorf("unexpected time for %v: %v vs. %v expected", input, actual, expect)
+		}
+	}
+
+	// Check nanoseconds.
+	for nanos := 1; nanos < 1e9; nanos <<= 1 {
+		input := fmt.Sprintf("2000-01-01T00:00:00.%09dZ", nanos)
+		expect, expectErr := time.Parse(time.RFC3339Nano, input)
+		actual, actualErr := Parse(input)
+		if (expectErr != nil) != (actualErr != nil) {
+			t.Errorf("unexpected error for %v: %v vs. %v expected", input, actualErr, expectErr)
+		} else if !actual.Equal(expect) {
+			t.Errorf("unexpected time for %v: %v vs. %v expected", input, actual, expect)
+		}
+	}
+
+	// Check with trailing zeroes omitted.
+	for n := 1; n < 1e9; n <<= 1 {
+		input := fmt.Sprintf("2000-01-01T00:00:00.%dZ", n)
+		expect, expectErr := time.Parse(time.RFC3339Nano, input)
+		actual, actualErr := Parse(input)
+		if (expectErr != nil) != (actualErr != nil) {
+			t.Errorf("unexpected error for %v: %v vs. %v expected", input, actualErr, expectErr)
+		} else if !actual.Equal(expect) {
+			t.Errorf("unexpected time for %v: %v vs. %v expected", input, actual, expect)
 		}
 	}
 }
@@ -147,6 +244,26 @@ func TestParseInvalid(t *testing.T) {
 		"1999-01-01T23:45:00 ", // missing timezone
 		"1999-01-01t23:45:00Z", // lowercase T
 		"1999-01-01T23:45:00z", // lowercase Z
+		"X999-01-01T23:45:00Z", // X in various positions
+		"1X99-01-01T23:45:00Z",
+		"19X9-01-01T23:45:00Z",
+		"199X-01-01T23:45:00Z",
+		"1999X01-01T23:45:00Z",
+		"1999-X1-01T23:45:00Z",
+		"1999-0X-01T23:45:00Z",
+		"1999-01X01T23:45:00Z",
+		"1999-01-X1T23:45:00Z",
+		"1999-01-0XT23:45:00Z",
+		"1999-01-01X23:45:00Z",
+		"1999-01-01TX3:45:00Z",
+		"1999-01-01T2X:45:00Z",
+		"1999-01-01T23X45:00Z",
+		"1999-01-01T23:X5:00Z",
+		"1999-01-01T23:4X:00Z",
+		"1999-01-01T23:45X00Z",
+		"1999-01-01T23:45:X0Z",
+		"1999-01-01T23:45:0XZ",
+		"1999-01-01T23:45:00X",
 
 		// 24 bytes
 		"XXXXXXXXXXXXXXXXXXXXXXXX",
@@ -182,6 +299,97 @@ func TestParseInvalid(t *testing.T) {
 		"1999-01-01T23:45:00.123 ", // missing timezone
 		"1999-01-01t23:45:00.123Z", // lowercase T
 		"1999-01-01T23:45:00.123z", // lowercase Z
+		"X999-01-01T23:45:00.123Z", // X in various positions
+		"1X99-01-01T23:45:00.123Z",
+		"19X9-01-01T23:45:00.123Z",
+		"199X-01-01T23:45:00.123Z",
+		"1999X01-01T23:45:00.123Z",
+		"1999-X1-01T23:45:00.123Z",
+		"1999-0X-01T23:45:00.123Z",
+		"1999-01X01T23:45:00.123Z",
+		"1999-01-X1T23:45:00.123Z",
+		"1999-01-0XT23:45:00.123Z",
+		"1999-01-01X23:45:00.123Z",
+		"1999-01-01TX3:45:00.123Z",
+		"1999-01-01T2X:45:00.123Z",
+		"1999-01-01T23X45:00.123Z",
+		"1999-01-01T23:X5:00.123Z",
+		"1999-01-01T23:4X:00.123Z",
+		"1999-01-01T23:45X00.123Z",
+		"1999-01-01T23:45:X0.123Z",
+		"1999-01-01T23:45:0X.123Z",
+		"1999-01-01T23:45:00X123Z",
+		"1999-01-01T23:45:00.X23Z",
+		"1999-01-01T23:45:00.1X3Z",
+		"1999-01-01T23:45:00.12XZ",
+		"1999-01-01T23:45:00.123X",
+
+		// 30 bytes
+		"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+		"000000000000000000000000000000",
+		"1900-02-29T00:00:00.123456789Z", // 28 days in month (not a leap year)
+		"2021-02-29T00:00:00.123456789Z", // 28 days in month (not a leap year)
+		"2021-02-30T00:00:00.123456789Z", // 28 days in month
+		"2021-02-31T00:00:00.123456789Z", // 28 days in month
+		"2021-04-31T00:00:00.123456789Z", // 30 days in month
+		"2021-06-31T00:00:00.123456789Z", // 30 days in month
+		"2021-09-31T00:00:00.123456789Z", // 30 days in month
+		"2021-11-31T00:00:00.123456789Z", // 30 days in month
+		"XXXX-13-01T00:00:00.123456789Z", // invalid year
+		"2000-13-01T00:00:00.123456789Z", // invalid month (1)
+		"2000-00-01T00:00:00.123456789Z", // invalid month (2)
+		"2000-XX-01T00:00:00.123456789Z", // invalid month (3)
+		"2000-12-32T00:00:00.123456789Z", // invalid day (1)
+		"2000-12-00T00:00:00.123456789Z", // invalid day (2)
+		"2000-12-XXT00:00:00.123456789Z", // invalid day (3)
+		"2000-12-31T24:00:00.123456789Z", // invalid hour (1)
+		"2000-12-31TXX:00:00.123456789Z", // invalid hour (2)
+		"2000-12-31T23:60:00.123456789Z", // invalid minute (1)
+		"2000-12-31T23:XX:00.123456789Z", // invalid minute (2)
+		"2000-12-31T23:59:60.123456789Z", // invalid second (1)
+		"2000-12-31T23:59:XX.123456789Z", // invalid second (2)
+		"2000-12-31T23:59:59.XXXXXXXXXZ", // invalid nanos
+		"1999-01-01 23:45:00.123456789Z", // missing T separator
+		"1999 01-01T23:45:00.123456789Z", // missing date separator (1)
+		"1999-01 01T23:45:00.123456789Z", // missing date separator (2)
+		"1999-01-01T23 45:00.123456789Z", // missing time separator (1)
+		"1999-01-01T23:45 00.123456789Z", // missing time separator (2)
+		"1999-01-01T23:45:00 123456789Z", // missing time separator (3)
+		"1999-01-01T23:45:00.123456789 ", // missing timezone
+		"1999-01-01t23:45:00.123456789Z", // lowercase T
+		"1999-01-01T23:45:00.123456789z", // lowercase Z
+		"X999-01-01T23:45:00.123456789Z", // X in various positions
+		"1X99-01-01T23:45:00.123456789Z",
+		"19X9-01-01T23:45:00.123456789Z",
+		"199X-01-01T23:45:00.123456789Z",
+		"1999X01-01T23:45:00.123456789Z",
+		"1999-X1-01T23:45:00.123456789Z",
+		"1999-0X-01T23:45:00.123456789Z",
+		"1999-01X01T23:45:00.123456789Z",
+		"1999-01-X1T23:45:00.123456789Z",
+		"1999-01-0XT23:45:00.123456789Z",
+		"1999-01-01X23:45:00.123456789Z",
+		"1999-01-01TX3:45:00.123456789Z",
+		"1999-01-01T2X:45:00.123456789Z",
+		"1999-01-01T23X45:00.123456789Z",
+		"1999-01-01T23:X5:00.123456789Z",
+		"1999-01-01T23:4X:00.123456789Z",
+		"1999-01-01T23:45X00.123456789Z",
+		"1999-01-01T23:45:X0.123456789Z",
+		"1999-01-01T23:45:0X.123456789Z",
+		"1999-01-01T23:45:00X123456789Z",
+		"1999-01-01T23:45:00.X23456789Z",
+		"1999-01-01T23:45:00.1X3456789Z",
+		"1999-01-01T23:45:00.12X456789Z",
+		"1999-01-01T23:45:00.123X56789Z",
+		"1999-01-01T23:45:00.1234X6789Z",
+		"1999-01-01T23:45:00.12345X789Z",
+		"1999-01-01T23:45:00.123456X89Z",
+		"1999-01-01T23:45:00.1234567X9Z",
+		"1999-01-01T23:45:00.12345678XZ",
+		"1999-01-01T23:45:00.123456789X",
+
+		"2000-01-01T00:00:00.Z", // missing number after decimal point
 	} {
 		t.Run(input, func(t *testing.T) {
 			ts, err := time.Parse(time.RFC3339Nano, input)
@@ -222,6 +430,6 @@ func BenchmarkParseNanoseconds(b *testing.B) {
 
 func BenchmarkParseInvalid(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Parse("2006-01-02T15:04:05X")
+		Parse("2006-01-02T15:04:05.XZ")
 	}
 }
